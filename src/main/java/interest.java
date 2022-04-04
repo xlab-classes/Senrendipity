34611aa8dbd275b66faf04dded9eb606ad4a4002
HaifengXiao-29 <70769700+HaifengXiao-29@users.noreply.github.com>
2022-04-04 02:21:45 -0400

    Sprint3#38 getInfo function

--- src/main/java/interest.java
+++ src/main/java/interest.java
@@ -2,6 +2,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class interest {
     private static final long  serialVersionUID = 1L;
@@ -26,7 +31,7 @@ public class interest {
 
     }
 
-    public void getInfo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    public void getInfo(HttpServletRequest request, HttpServletResponse response) throws Exception {
         /*
            1.拿到wjj传输过来的数据
            4.先去label_table里看wjj传过来的label是否存在
@@ -45,5 +50,59 @@ public class interest {
               失败传啥
          */
 
+        String username = request.getParameter("username");
+        String interest = request.getParameter("interest");
+
+        PrintWriter check = response.getWriter();
+
+        app_im serv = new app_im();
+        info_im info = new info_im();
+        int user_id = serv.getId(username);
+        String label_id;
+
+        if(info.getLabel(user_id).contains(interest)) {
+            label_id = interest;
+            check.print("0");
+        }
+        else {
+            info.addLabel();
+            label_id = Integer.toString(info.search_id());
+        }
+
+        if(info.interest_num(info.getLabel(user_id)) == 10) {
+            check.write("-1");
+        }
+        else if(info.interest_num(info.getLabel(user_id)) == 0) {
+            info.addInfo(user_id, interest, 0);
+            check.write("1");
+        }
+        else {
+            check.write("1");
+        }
+
+
     }
+    public static void main(String[] args) throws Exception {
+        String username = "hx2029";
+        String interest = "6";
+
+        app_im serv = new app_im();
+        info_im info = new info_im();
+        int user_id = serv.getId(username);
+        String label_id;
+
+        if(info.getLabel(user_id).contains(interest)) {
+            label_id = interest;
+            System.out.println("exited");
+        }
+        else {
+            info.addLabel();
+            info.addInfo(user_id, interest, 0);
+            label_id = Integer.toString(info.search_id(interest));
+        }
+        System.out.println(info.getLabel(user_id));
+
+
+    }
+
 }
